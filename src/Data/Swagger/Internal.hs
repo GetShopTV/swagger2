9a5e6b9c727480c5f33f4d9db8b865e274dcc905
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-11-06 17:48:26 +0300

    Add data model for Responses object

--- src/Data/Swagger/Internal.hs
+++ src/Data/Swagger/Internal.hs
@@ -399,7 +399,24 @@ data SwaggerItems = SwaggerItems
   , swaggerItemsMultipleOf :: Maybe Integer
   } deriving (Show)
 
+-- | A container for the expected responses of an operation.
+-- The container maps a HTTP response code to the expected response.
+-- It is not expected from the documentation to necessarily cover all possible HTTP response codes,
+-- since they may not be known in advance.
+-- However, it is expected from the documentation to cover a successful operation response and any known errors.
 data SwaggerResponses = SwaggerResponses
+  { -- | The documentation of responses other than the ones declared for specific HTTP response codes.
+    -- It can be used to cover undeclared responses.
+    swaggerResponsesDefault :: SwaggerResponse
+
+    -- | Any HTTP status code can be used as the property name (one property per HTTP status code).
+    -- Describes the expected response for those HTTP status codes.
+  , swaggerResponsesResponses :: HashMap HttpStatusCode SwaggerResponse
+  } deriving (Show)
+
+type HttpStatusCode = Int
+
+data SwaggerResponse = SwaggerResponse
   deriving (Show)
 
 data SwaggerExtension = SwaggerExtension
