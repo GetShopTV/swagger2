54752635a27e25f7377661d5d1de211b8b2d5f68
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-11-08 00:03:53 +0300

    Add test for Operation object

--- test/Data/SwaggerSpec.hs
+++ test/Data/SwaggerSpec.hs
@@ -1,13 +1,33 @@
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 module Data.SwaggerSpec where
 
 import Data.Aeson
 import Data.Aeson.QQ
+import qualified Data.HashMap.Strict as HashMap
+import Data.Maybe
+import qualified Data.Vector as Vector
+
 import Data.Swagger
 
 import Test.Hspec
 import Test.Hspec.HUnit
+
+isSubJSON :: Value -> Value -> Bool
+isSubJSON Null _ = True
+isSubJSON (Object x) (Object y) = HashMap.keys x == HashMap.keys i && and i
+  where
+    i = HashMap.intersectionWith isSubJSON x y
+isSubJSON (Array xs) (Array ys) = Vector.length xs == Vector.length ys && and (Vector.zipWith isSubJSON xs ys)
+isSubJSON x y = x == y
+
+(<~>) :: (Eq a, Show a, ToJSON a, FromJSON a) => a -> Value -> Spec
+x <~> json = do
+  it "encodes correctly (probably with extra properties)" $ do
+    toJSON x `shouldSatisfy` (json `isSubJSON`)
+  it "decodes correctly" $ do
+    fromJSON json `shouldBe` Success x
 
 (<=>) :: (Eq a, Show a, ToJSON a, FromJSON a) => a -> Value -> Spec
 x <=> json = do
@@ -18,6 +38,7 @@ x <=> json = do
 
 spec :: Spec
 spec = do
+  describe "Operation Object" $ operationExample <~> operationExampleJSON
   describe "License Object" $ licenseExample <=> licenseExampleJSON
   describe "Contact Object" $ contactExample <=> contactExampleJSON
   describe "Info Object" $ infoExample <=> infoExampleJSON
@@ -94,5 +115,143 @@ licenseExampleJSON = [aesonQQ|
 {
   "name": "Apache 2.0",
   "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+}
+|]
+
+
+-- =======================================================================
+-- Operation object
+-- =======================================================================
+
+operationExample :: SwaggerOperation
+operationExample = SwaggerOperation
+  { swaggerOperationTags = ["pet"]
+  , swaggerOperationSummary = Just "Updates a pet in the store with form data"
+  , swaggerOperationDescription = Just ""
+  , swaggerOperationOperationId = Just "updatePetWithForm"
+  , swaggerOperationConsumes = Just (SwaggerMimeList ["application/x-www-form-urlencoded"])
+  , swaggerOperationProduces = Just (SwaggerMimeList ["application/json", "application/xml"])
+  , swaggerOperationParameters = params
+  , swaggerOperationResponses = responses
+  , swaggerOperationSecurity = security
+  , swaggerOperationExternalDocs = Nothing
+  , swaggerOperationSchemes = Nothing
+  , swaggerOperationDeprecated = False
+  }
+  where
+    security = [SwaggerSecurityRequirement [("petstore_auth", ["write:pets", "read:pets"])]]
+
+    responses = SwaggerResponses
+      { swaggerResponsesDefault = Nothing
+      , swaggerResponsesResponses =
+          [ (200, SwaggerResponse
+                    { swaggerResponseDescription = "Pet updated."
+                    , swaggerResponseSchema = Nothing
+                    , swaggerResponseHeaders = mempty
+                    , swaggerResponseExamples = Nothing } )
+          , (405, SwaggerResponse
+                    { swaggerResponseDescription = "Invalid input"
+                    , swaggerResponseSchema = Nothing
+                    , swaggerResponseHeaders = mempty
+                    , swaggerResponseExamples = Nothing } ) ] }
+
+    params =
+      [ SwaggerParameter
+          { swaggerParameterName = "petId"
+          , swaggerParameterDescription = Just "ID of pet that needs to be updated"
+          , swaggerParameterRequired = True
+          , swaggerParameterSchema = SwaggerParameterOther (stringSchema SwaggerParameterPath) }
+      , SwaggerParameter
+          { swaggerParameterName = "name"
+          , swaggerParameterDescription = Just "Updated name of the pet"
+          , swaggerParameterRequired = False
+          , swaggerParameterSchema = SwaggerParameterOther (stringSchema SwaggerParameterFormData) }
+      , SwaggerParameter
+          { swaggerParameterName = "status"
+          , swaggerParameterDescription = Just "Updated status of the pet"
+          , swaggerParameterRequired = False
+          , swaggerParameterSchema = SwaggerParameterOther (stringSchema SwaggerParameterFormData) }
+      ]
+
+    stringSchema i = SwaggerParameterOtherSchema
+      { swaggerParameterOtherSchemaIn = i
+      , swaggerParameterOtherSchemaType = SwaggerParamString
+      , swaggerParameterOtherSchemaFormat = Nothing
+      , swaggerParameterOtherSchemaAllowEmptyValue = False
+      , swaggerParameterOtherSchemaItems = Nothing
+      , swaggerParameterOtherSchemaCollectionFormat = Nothing
+      , swaggerParameterOtherSchemaCommon = emptySchemaCommon
+      }
+
+    emptySchemaCommon = SwaggerSchemaCommon
+      { swaggerSchemaDefault = Nothing
+      , swaggerSchemaMaximum = Nothing
+      , swaggerSchemaExclusiveMaximum = Nothing
+      , swaggerSchemaMinimum = Nothing
+      , swaggerSchemaExclusiveMinimum = Nothing
+      , swaggerSchemaMaxLength = Nothing
+      , swaggerSchemaMinLength = Nothing
+      , swaggerSchemaPattern = Nothing
+      , swaggerSchemaMaxItems = Nothing
+      , swaggerSchemaMinItems = Nothing
+      , swaggerSchemaUniqueItems = Nothing
+      , swaggerSchemaEnum = Nothing
+      , swaggerSchemaMultipleOf = Nothing }
+
+operationExampleJSON :: Value
+operationExampleJSON = [aesonQQ|
+{
+  "tags": [
+    "pet"
+  ],
+  "summary": "Updates a pet in the store with form data",
+  "description": "",
+  "operationId": "updatePetWithForm",
+  "consumes": [
+    "application/x-www-form-urlencoded"
+  ],
+  "produces": [
+    "application/json",
+    "application/xml"
+  ],
+  "parameters": [
+    {
+      "name": "petId",
+      "in": "path",
+      "description": "ID of pet that needs to be updated",
+      "required": true,
+      "type": "string"
+    },
+    {
+      "name": "name",
+      "in": "formData",
+      "description": "Updated name of the pet",
+      "required": false,
+      "type": "string"
+    },
+    {
+      "name": "status",
+      "in": "formData",
+      "description": "Updated status of the pet",
+      "required": false,
+      "type": "string"
+    }
+  ],
+  "responses": {
+    "200": {
+      "description": "Pet updated."
+    },
+    "405": {
+      "description": "Invalid input"
+    }
+  },
+  "security": [
+    {
+      "petstore_auth": [
+        "write:pets",
+        "read:pets"
+      ]
+    }
+  ]
 }
 |]
