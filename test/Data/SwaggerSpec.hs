fc398d525bd7b2f346249ce896d83cd91a6d3495
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-11-07 19:07:03 +0300

    Add Contact and License tests, simplify spec

--- test/Data/SwaggerSpec.hs
+++ test/Data/SwaggerSpec.hs
@@ -9,19 +9,35 @@ import Data.Swagger
 import Test.Hspec
 import Test.Hspec.HUnit
 
+(<=>) :: (Eq a, Show a, ToJSON a, FromJSON a) => a -> Value -> Spec
+x <=> json = do
+  it "encodes correctly" $ do
+    toJSON x `shouldBe` json
+  it "decodes correctly" $ do
+    fromJSON json `shouldBe` Success x
+
+spec :: Spec
+spec = do
+  describe "License Object" $ licenseExample <=> licenseExampleJSON
+  describe "Contact Object" $ contactExample <=> contactExampleJSON
+  describe "Info Object" $ infoExample <=> infoExampleJSON
+
+main :: IO ()
+main = hspec spec
+
+-- =======================================================================
+-- Info object
+-- =======================================================================
+
 infoExample :: SwaggerInfo
 infoExample = SwaggerInfo
   { swaggerInfoTitle = "Swagger Sample App"
   , swaggerInfoDescription = Just "This is a sample server Petstore server."
   , swaggerInfoTermsOfService = Just "http://swagger.io/terms/"
-  , swaggerInfoContact = Just contact
-  , swaggerInfoLicense = Just license
+  , swaggerInfoContact = Just contactExample
+  , swaggerInfoLicense = Just licenseExample
   , swaggerInfoVersion = "1.0.1" }
   where
-    contact = SwaggerContact
-      { swaggerContactName = Just "API Support"
-      , swaggerContactUrl = Just (URL "http://www.swagger.io/support")
-      , swaggerContactEmail = Just "support@swagger.io" }
     license = SwaggerLicense
       { swaggerLicenseName = "Apache 2.0"
       , swaggerLicenseUrl = Just (URL "http://www.apache.org/licenses/LICENSE-2.0.html") }
@@ -45,14 +61,38 @@ infoExampleJSON = [aesonQQ|
 }
 |]
 
-spec :: Spec
-spec = do
-  describe "Info Object" $ do
-    it "encodes correctly" $ do
-      toJSON infoExample `shouldBe` infoExampleJSON
-    it "decodes correctly" $ do
-      fromJSON infoExampleJSON `shouldBe` Success infoExample
+-- =======================================================================
+-- Contact object
+-- =======================================================================
 
-main :: IO ()
-main = hspec spec
+contactExample :: SwaggerContact
+contactExample = SwaggerContact
+  { swaggerContactName = Just "API Support"
+  , swaggerContactUrl = Just (URL "http://www.swagger.io/support")
+  , swaggerContactEmail = Just "support@swagger.io" }
 
+contactExampleJSON :: Value
+contactExampleJSON = [aesonQQ|
+{
+  "name": "API Support",
+  "url": "http://www.swagger.io/support",
+  "email": "support@swagger.io"
+}
+|]
+
+-- =======================================================================
+-- License object
+-- =======================================================================
+
+licenseExample :: SwaggerLicense
+licenseExample = SwaggerLicense
+  { swaggerLicenseName = "Apache 2.0"
+  , swaggerLicenseUrl = Just (URL "http://www.apache.org/licenses/LICENSE-2.0.html") }
+
+licenseExampleJSON :: Value
+licenseExampleJSON = [aesonQQ|
+{
+  "name": "Apache 2.0",
+  "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+}
+|]
