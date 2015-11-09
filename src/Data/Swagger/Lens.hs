6de52f3050100b7e8ee5293d4479f32d95d34da6
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-11-09 13:19:38 +0300

    Add HasSwaggerSchemaCommon helper class for simpler lenses

--- src/Data/Swagger/Lens.hs
+++ src/Data/Swagger/Lens.hs
@@ -39,3 +39,67 @@ makeLenses ''SwaggerApiKeyParams
 makeLenses ''SwaggerOAuth2Params
 makeLenses ''SwaggerExternalDocs
 
+-- =======================================================================
+-- Helper classy lenses
+-- =======================================================================
+
+class HasSwaggerSchemaCommon s where
+  schemaCommon :: Lens' s SwaggerSchemaCommon
+
+instance HasSwaggerSchemaCommon SwaggerParameter where
+  schemaCommon = swaggerParameterSchema.schemaCommon
+
+instance HasSwaggerSchemaCommon SwaggerParameterSchema where
+  schemaCommon = paramSchemaEither . choosing schemaCommon schemaCommon
+    where
+      paramSchemaEither = iso toEither fromEither
+      toEither (SwaggerParameterBody  x) = Left x
+      toEither (SwaggerParameterOther y) = Right y
+      fromEither (Left  x) = SwaggerParameterBody x
+      fromEither (Right y) = SwaggerParameterOther y
+
+instance HasSwaggerSchemaCommon SwaggerSchema where schemaCommon = swaggerSchemaCommon
+instance HasSwaggerSchemaCommon SwaggerParameterOtherSchema where schemaCommon = swaggerParameterOtherSchemaCommon
+instance HasSwaggerSchemaCommon SwaggerItems where schemaCommon = swaggerItemsCommon
+instance HasSwaggerSchemaCommon SwaggerHeader where schemaCommon = swaggerHeaderCommon
+instance HasSwaggerSchemaCommon SwaggerSchemaCommon where schemaCommon = id
+
+schemaDefault :: HasSwaggerSchemaCommon s => Lens' s (Maybe Value)
+schemaDefault = schemaCommon.swaggerSchemaDefault
+
+schemaMaximum :: HasSwaggerSchemaCommon s => Lens' s (Maybe Integer)
+schemaMaximum = schemaCommon.swaggerSchemaMaximum
+
+schemaExclusiveMaximum :: HasSwaggerSchemaCommon s => Lens' s (Maybe Bool)
+schemaExclusiveMaximum = schemaCommon.swaggerSchemaExclusiveMaximum
+
+schemaMinimum :: HasSwaggerSchemaCommon s => Lens' s (Maybe Integer)
+schemaMinimum = schemaCommon.swaggerSchemaMinimum
+
+schemaExclusiveMinimum :: HasSwaggerSchemaCommon s => Lens' s (Maybe Bool)
+schemaExclusiveMinimum = schemaCommon.swaggerSchemaExclusiveMinimum
+
+schemaMaxLength :: HasSwaggerSchemaCommon s => Lens' s (Maybe Integer)
+schemaMaxLength = schemaCommon.swaggerSchemaMaxLength
+
+schemaMinLength :: HasSwaggerSchemaCommon s => Lens' s (Maybe Integer)
+schemaMinLength = schemaCommon.swaggerSchemaMinLength
+
+schemaPattern :: HasSwaggerSchemaCommon s => Lens' s (Maybe Text)
+schemaPattern = schemaCommon.swaggerSchemaPattern
+
+schemaMaxItems :: HasSwaggerSchemaCommon s => Lens' s (Maybe Integer)
+schemaMaxItems = schemaCommon.swaggerSchemaMaxItems
+
+schemaMinItems :: HasSwaggerSchemaCommon s => Lens' s (Maybe Integer)
+schemaMinItems = schemaCommon.swaggerSchemaMinItems
+
+schemaUniqueItems :: HasSwaggerSchemaCommon s => Lens' s (Maybe Bool)
+schemaUniqueItems = schemaCommon.swaggerSchemaUniqueItems
+
+schemaEnum :: HasSwaggerSchemaCommon s => Lens' s (Maybe [Value])
+schemaEnum = schemaCommon.swaggerSchemaEnum
+
+schemaMultipleOf :: HasSwaggerSchemaCommon s => Lens' s (Maybe Integer)
+schemaMultipleOf = schemaCommon.swaggerSchemaMultipleOf
+
