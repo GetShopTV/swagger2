9b237b9f1a73df9ab6972ce46d426853125f0644
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-11-24 00:14:41 +0300

    Add unary data type tests

--- test/Data/Swagger/SchemaSpec.hs
+++ test/Data/Swagger/SchemaSpec.hs
@@ -24,7 +24,11 @@ spec = do
   describe "Generic ToSchema" $ do
     context "Person" $ checkToSchema (Proxy :: Proxy Person) personSchemaJSON
     context "ISPair" $ checkToSchema (Proxy :: Proxy ISPair) ispairSchemaJSON
-    context "Point (custom fieldLabelModifier)" $ checkToSchema (Proxy :: Proxy Point) pointSchemaJSON
+    context "Point (fieldLabelModifier)" $ checkToSchema (Proxy :: Proxy Point) pointSchemaJSON
+    context "Unary records" $ do
+      context "Email (unwrapUnaryRecords)"  $ checkToSchema (Proxy :: Proxy Email)  emailSchemaJSON
+      context "UserId (non-record newtype)" $ checkToSchema (Proxy :: Proxy UserId) userIdSchemaJSON
+      context "Player (unary record)" $ checkToSchema (Proxy :: Proxy Player) playerSchemaJSON
   describe "toSchemaBoundedEnum" $ do
     context "Color" $ checkToSchema (Proxy :: Proxy Color) colorSchemaJSON
 
@@ -118,3 +122,63 @@ colorSchemaJSON = [aesonQQ|
 }
 |]
 
+-- ========================================================================
+-- Email (newtype with unwrapUnaryRecords set to True)
+-- ========================================================================
+
+newtype Email = Email { getEmail :: String }
+  deriving (Generic)
+
+instance ToSchema Email where
+  toSchema = genericToSchema defaultSchemaOptions
+    { unwrapUnaryRecords = True }
+
+emailSchemaJSON :: Value
+emailSchemaJSON = [aesonQQ|
+{
+  "type": "string"
+}
+|]
+
+-- ========================================================================
+-- UserId (non-record newtype)
+-- ========================================================================
+
+newtype UserId = UserId Integer
+  deriving (Generic, ToSchema)
+
+userIdSchemaJSON :: Value
+userIdSchemaJSON = [aesonQQ|
+{
+  "type": "integer"
+}
+|]
+
+-- ========================================================================
+-- Player (record newtype)
+-- ========================================================================
+
+newtype Player = Player
+  { position :: Point
+  } deriving (Generic, ToSchema)
+
+playerSchemaJSON :: Value
+playerSchemaJSON = [aesonQQ|
+{
+  "type": "object",
+  "properties":
+    {
+      "position":
+        {
+          "type": "object",
+          "properties":
+            {
+              "x": { "type": "number" },
+              "y": { "type": "number" }
+            },
+          "required": [ "x", "y" ]
+        }
+    },
+  "required": ["position"]
+}
+|]
