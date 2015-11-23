0703c199116ce71b8766caca9137f62deefc92ab
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-11-23 18:51:35 +0300

    Add test with custom fieldLabelModifier

--- test/Data/Swagger/SchemaSpec.hs
+++ test/Data/Swagger/SchemaSpec.hs
@@ -6,10 +6,13 @@ module Data.Swagger.SchemaSpec where
 
 import Data.Aeson
 import Data.Aeson.QQ
+import Data.Char
 import Data.Proxy
 import GHC.Generics
 
 import Data.Swagger
+import Data.Swagger.Schema
+
 import SpecCommon
 import Test.Hspec
 
@@ -21,6 +24,7 @@ spec = do
   describe "Generic ToSchema" $ do
     context "Person" $ checkToSchema (Proxy :: Proxy Person) personSchemaJSON
     context "ISPair" $ checkToSchema (Proxy :: Proxy ISPair) ispairSchemaJSON
+    context "Point (custom fieldLabelModifier)" $ checkToSchema (Proxy :: Proxy Point) pointSchemaJSON
 
 main :: IO ()
 main = hspec spec
@@ -57,6 +61,28 @@ ispairSchemaJSON = [aesonQQ|
       { "type": "integer" },
       { "type": "string"  }
     ]
+}
+|]
+
+data Point = Point
+  { pointX :: Double
+  , pointY :: Double
+  } deriving (Generic)
+
+instance ToSchema Point where
+  toSchema = genericToSchema defaultSchemaOptions
+    { fieldLabelModifier = map toLower . drop (length "point") }
+
+pointSchemaJSON :: Value
+pointSchemaJSON = [aesonQQ|
+{
+  "type": "object",
+  "properties":
+    {
+      "x": { "type": "number" },
+      "y": { "type": "number" }
+    },
+  "required": ["x", "y"]
 }
 |]
 
