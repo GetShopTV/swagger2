3c39be66dccc6db18a9ea6152445f6eda1696924
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-12-09 15:38:41 +0300

    Add ToParamSchema test with constructorTagModifier

--- test/Data/Swagger/ParamSchemaSpec.hs
+++ test/Data/Swagger/ParamSchemaSpec.hs
@@ -24,6 +24,7 @@ spec :: Spec
 spec = do
   describe "Generic ToParamSchema" $ do
     context "Color (bounded enum)" $ checkToParamSchema (Proxy :: Proxy Color) colorSchemaJSON
+    context "Status (constructorTagModifier)" $ checkToParamSchema (Proxy :: Proxy Status) statusSchemaJSON
     context "Unary records" $ do
       context "Email (unary record)"  $ checkToParamSchema (Proxy :: Proxy Email)  emailSchemaJSON
       context "UserId (non-record newtype)" $ checkToParamSchema (Proxy :: Proxy UserId) userIdSchemaJSON
@@ -45,6 +46,24 @@ colorSchemaJSON = [aesonQQ|
 {
   "type": "string",
   "enum": ["Red", "Green", "Blue"]
+}
+|]
+
+-- ========================================================================
+-- Status (constructorTagModifier)
+-- ========================================================================
+
+data Status = StatusOk | StatusError deriving (Generic)
+
+instance ToParamSchema Status where
+  toParamSchema = genericToParamSchema defaultSchemaOptions
+    { constructorTagModifier = map toLower . drop (length "Status") }
+
+statusSchemaJSON :: Value
+statusSchemaJSON = [aesonQQ|
+{
+  "type": "string",
+  "enum": ["ok", "error"]
 }
 |]
 
