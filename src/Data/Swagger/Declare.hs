64e2ba4e9eea59338e76b4e882ac7afb45d30ae7
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-12-12 01:45:21 +0300

    Add some documentations and laws for Declare

--- src/Data/Swagger/Declare.hs
+++ src/Data/Swagger/Declare.hs
@@ -9,6 +9,18 @@ import Control.Monad.Trans
 import Data.Functor.Identity
 import Data.Monoid
 
+-- | A declare monad transformer parametrized by:
+--
+--  * @d@ — the output to accumulate (declarations);
+--
+--  * @m@ — the inner monad.
+--
+-- This monad transformer is similar to both state and writer monad transformers.
+-- Thus it can be seen as
+--
+--  * a restricted append-only version of a state monad transformer or
+--
+--  * a writer monad transformer with the extra ability to read all previous output.
 newtype DeclareT d m a = DeclareT { runDeclareT :: d -> m (d, a) }
   deriving (Functor)
 
@@ -29,14 +41,28 @@ instance (Monad m, Monoid d) => Monad (DeclareT d m) where
 instance Monoid d => MonadTrans (DeclareT d) where
   lift m = DeclareT (\_ -> (,) mempty <$> m)
 
+-- |
+-- Laws:
+--
+-- @
+-- declare mempty == return ()
+-- declare x >> declare y == declare (x <> y)
+--
+-- declare x >> look == fmap (<> x) look
+--
+-- look >> f == f
+-- @
 class Monad m => MonadDeclare d m | m -> d where
+  -- | @'declare' x@ is an action that produces the output @x@.
   declare :: d -> m ()
+  -- | @'look'@ is an action that returns all the output so far.
   look :: m d
 
 instance (Monad m, Monoid d) => MonadDeclare d (DeclareT d m) where
   declare d = DeclareT (\_ -> return (d, ()))
   look = DeclareT (\d -> return (mempty, d))
 
+-- | Retrieve a function of all the output so far.
 looks :: MonadDeclare d m => (d -> a) -> m a
 looks f = f <$> look
 
@@ -49,6 +75,14 @@ execDeclareT (DeclareT f) d = fst `liftM` f d
 undeclareT :: (Monad m, Monoid d) => DeclareT d m a -> m a
 undeclareT = flip evalDeclareT mempty
 
+-- | A declare monad parametrized by @d@ — the output to accumulate (declarations).
+--
+-- This monad is similar to both state and writer monads.
+-- Thus it can be seen as
+--
+--  * a restricted append-only version of a state monad or
+--
+--  * a writer monad with the extra ability to read all previous output.
 type Declare d = DeclareT d Identity
 
 runDeclare :: Declare d a -> d -> (d, a)
