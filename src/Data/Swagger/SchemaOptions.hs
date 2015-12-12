900515b8c3a80cd5f0e47f334d3fd457c681f10b
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-12-12 15:42:12 +0300

    Remove useReferences from SchemaOptions

--- src/Data/Swagger/SchemaOptions.hs
+++ src/Data/Swagger/SchemaOptions.hs
@@ -11,22 +11,6 @@ data SchemaOptions = SchemaOptions
     -- | If @'True'@ the constructors of a datatype, with all nullary constructors,
     -- will be encoded to a string enumeration schema with the constructor tags as possible values.
   , allNullaryToStringTag :: Bool
-    -- | If @'True'@ direct subschemas will be referenced if possible (rather than inlined).
-    -- Note that this option does not influence nested schemas, e.g. for these types
-    --
-    -- @
-    -- data Object = Object String deriving Generic
-    -- instance ToSchema Object
-    --
-    -- newtype Objects = Objects [Object] deriving Generic
-    -- instance ToSchema Objects where
-    --    toNamedSchema = genericToNamedSchema defaultSchemaOptions
-    --      { useReferences = False }
-    -- @
-    --
-    -- Schema for @Objects@ __will not__ inline @Object@ schema because
-    -- it is nested in a @[]@ schema.
-  , useReferences :: Bool
     -- | Hide the field name when a record constructor has only one field, like a newtype.
   , unwrapUnaryRecords :: Bool
   }
@@ -39,7 +23,6 @@ data SchemaOptions = SchemaOptions
 -- , 'constructorTagModifier' = id
 -- , 'datatypeNameModifier'   = id
 -- , 'allNullaryToStringTag'  = True
--- , 'useReferences'          = True
 -- , 'unwrapUnaryRecords'     = False
 -- }
 -- @
@@ -49,7 +32,6 @@ defaultSchemaOptions = SchemaOptions
   , constructorTagModifier = id
   , datatypeNameModifier = id
   , allNullaryToStringTag = True
-  , useReferences = True
   , unwrapUnaryRecords = False
   }
 
